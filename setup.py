72dd4f95012a2850cb5d470bdceb96e6ed3104ab
Eugene Mayer <white@pfel.ru>
2021-08-01 05:50:09 +0300

    Ещё маленько большого и значительного

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements():
 
 setup(
     name='apito',
-    version='0.3.1',
+    version='0.3.2',
     packages=['apito', 'apito.models'],
     url='https://github.com/WhiteApfel/apito',
     license='MPL 2.0',
